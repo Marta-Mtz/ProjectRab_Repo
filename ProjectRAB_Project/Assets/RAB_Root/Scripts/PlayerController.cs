de3b129d9cac5a828c2be4754788536538e96260
corpsegin <eidenzuluaga64@gmail.com>
2025-10-27 22:30:16 +0100

    ahora si

--- ProjectRAB_Project/Assets/RAB_Root/Scripts/PlayerController.cs
+++ ProjectRAB_Project/Assets/RAB_Root/Scripts/PlayerController.cs
@@ -12,11 +12,7 @@ public class PlayerController : MonoBehaviour
     [Header("Movement Parameters")]
     public float speed = 10;
     public float rotationSpeed = 10f;
-<<<<<<< HEAD:ProjectRAB_Project/Assets/RAB_Root/Scripts/PlayerController2.cs
-    public Vector2 moveInput; //Almac n del input de movimiento de los perif ricos que usamos para jugar
-=======
-    public Vector2 moveInput; //Almac�n del input de movimiento de los perif�ricos que usamos para jugar
->>>>>>> parent of 4115d6e (Arreglado el bug de la camara y los controles :)):ProjectRAB_Project/Assets/RAB_Root/Scripts/PlayerController.cs
+    public Vector2 moveInput; //Almacén del input de movimiento de los periféricos que usamos para jugar
 
     [Header("Jump Parameters")]
     public float jumpForce = 6;
@@ -35,22 +31,14 @@ public class PlayerController : MonoBehaviour
     [Header("Sound Configuration")]
     public AudioClip[] soundCollection;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
-<<<<<<< HEAD:ProjectRAB_Project/Assets/RAB_Root/Scripts/PlayerController2.cs
-        playerRb.sleepThreshold = 0f;
-=======
         playerRb.sleepThreshold = 0f; // Evita que la bola se duerma
->>>>>>> parent of 4115d6e (Arreglado el bug de la camara y los controles :)):ProjectRAB_Project/Assets/RAB_Root/Scripts/PlayerController.cs
     }
 
-    // Update is called once per frame
     void Update()
     {
-        //CinematicMovement();
-        //Respawn por altura
         if (transform.position.y <= fallLimit)
         {
             Respawn();
@@ -59,15 +47,14 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        // Movimiento físico
         PhysicalMovement();
 
         float smoothFactor = 0.1f;
         Vector3 targetVelocity = new Vector3(moveInput.x * speed, playerRb.linearVelocity.y, moveInput.y * speed);
         playerRb.linearVelocity = Vector3.Lerp(playerRb.linearVelocity, targetVelocity, smoothFactor);
 
-        if(isJumping && Time.time - jumpStartTime >= maxJumpTime)
-    {
+        if (isJumping && Time.time - jumpStartTime >= maxJumpTime)
+        {
             isJumping = false;
         }
     }
@@ -76,75 +63,56 @@ public class PlayerController : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Ground"))
         {
-            isGrounded = true; //Devuelve la capacidad de saltar
+            isGrounded = true;
         }
         if (collision.gameObject.CompareTag("Obstacle"))
         {
             Respawn();
         }
-      
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("PowerUp"))
-    {
-            
+        {
             playerRb.linearVelocity = new Vector3(playerRb.linearVelocity.x, 0, playerRb.linearVelocity.z);
             playerRb.AddForce(Vector3.up * powerUpJumpForce, ForceMode.Impulse);
-
-            
         }
+    }
 
-    }  
-   
     private void OnCollisionExit(Collision collision)
     {
         if (collision.gameObject.CompareTag("Ground"))
         {
-            isGrounded = false; 
+            isGrounded = false;
         }
     }
 
-
-
     void CinematicMovement()
     {
-<<<<<<< HEAD:ProjectRAB_Project/Assets/RAB_Root/Scripts/PlayerController2.cs
-        //Movimiento = (Direcci n * velocidad * input)
-=======
-        //Movimiento = (Direcci�n * velocidad * input)
->>>>>>> parent of 4115d6e (Arreglado el bug de la camara y los controles :)):ProjectRAB_Project/Assets/RAB_Root/Scripts/PlayerController.cs
-        //Necesitais multiplicar el movimiento por Time.deltaTime
+        //Movimiento = (Dirección * velocidad * input)
         transform.Translate(Vector3.right * speed * moveInput.x * Time.deltaTime);
         transform.Translate(Vector3.forward * speed * moveInput.y * Time.deltaTime);
     }
 
     void PhysicalMovement()
     {
-<<<<<<< HEAD:ProjectRAB_Project/Assets/RAB_Root/Scripts/PlayerController2.cs
-        //A adir una fuerza al rigidbody = (Direcci n * velocidad * input)
-=======
-        //A�adir una fuerza al rigidbody = (Direcci�n * velocidad * input)
->>>>>>> parent of 4115d6e (Arreglado el bug de la camara y los controles :)):ProjectRAB_Project/Assets/RAB_Root/Scripts/PlayerController.cs
+        //Añadir una fuerza al rigidbody = (Dirección * velocidad * input)
         playerRb.AddForce(Vector3.right * speed * moveInput.x);
         playerRb.AddForce(Vector3.forward * speed * moveInput.y);
     }
 
-
     void Jump()
     {
-        isGrounded = false; // ya no puede saltar otra vez hasta tocar el suelo
-        playerRb.linearVelocity = new Vector3(playerRb.linearVelocity.x, 0, playerRb.linearVelocity.z); // reinicia velocidad vertical
+        isGrounded = false;
+        playerRb.linearVelocity = new Vector3(playerRb.linearVelocity.x, 0, playerRb.linearVelocity.z);
         playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
         PlaySFX(0);
     }
 
     void Respawn()
     {
-        //Sustituir el transform.position del player por el del punto de respawn
         transform.position = respawnPoint.position;
-        //Resetear el valor de aceleraci n del rigidbody
         playerRb.linearVelocity = Vector3.zero;
         PlaySFX(2);
     }
@@ -161,25 +129,17 @@ public class PlayerController : MonoBehaviour
         moveInput = context.ReadValue<Vector2>();
     }
 
-    public void OnJump(InputAction.CallbackContext context) 
+    public void OnJump(InputAction.CallbackContext context)
     {
-
-        if (context.performed && isGrounded == true)
+        if (context.performed && isGrounded)
         {
             isGrounded = false;
             isJumping = true;
             jumpStartTime = Time.time;
-            isGrounded = false;
 
             Jump();
-     
         }
-        
-
     }
-
-
-
 
     #endregion
 }
