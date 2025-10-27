00bfb8c4118f9c8d7b609cfb6e9be30fe517527f
corpsegin <eidenzuluaga64@gmail.com>
2025-10-27 19:44:26 +0100

    dgsegv

--- ProjectRAB_Project/Assets/RAB_Root/Scripts/PlayerController.cs
+++ ProjectRAB_Project/Assets/RAB_Root/Scripts/PlayerController.cs
@@ -35,7 +35,7 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
-        playerRb.sleepThreshold = 0f; // Evita que la bola se duerma
+        playerRb.sleepThreshold = 0f;
     }
 
     // Update is called once per frame
@@ -94,7 +94,7 @@ public class PlayerController : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Ground"))
         {
-            isGrounded = false; // ❌ Cuando sales del suelo, ya no puede saltar
+            isGrounded = false; 
         }
     }
 
