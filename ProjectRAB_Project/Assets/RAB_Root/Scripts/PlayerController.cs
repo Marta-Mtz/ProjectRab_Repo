894539d56ff208d75ad4ebb2be8ffa2153d30afa
noes-elena <elenamunozpardo6@gmail.com>
2025-10-28 16:16:51 +0100

    Prefab estanterias tutorial

--- ProjectRAB_Project/Assets/RAB_Root/Scripts/PlayerController.cs
+++ ProjectRAB_Project/Assets/RAB_Root/Scripts/PlayerController.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
 
-[RequireComponent(typeof(Rigidbody))]
+public class PlayerController: MonoBehaviour
 {
 
     [Header("Editor References")]
